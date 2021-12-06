57ca9683eb5be93b899ac0b7c6bd6207e11bab26
wentingzoe <ywtzoe@gmail.com>
2021-12-06 16:19:43 -0500

    install bootstrap and creat Url show

--- express_server.js
+++ express_server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,21 +12,28 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.get("/hello", (req, res) => {
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase };
+  res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
  });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
