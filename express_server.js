0a9b02f0022121c3db0e0beab06bbb93a19a5284
wentingzoe <ywtzoe@gmail.com>
2021-12-07 22:39:07 -0500

    create login and logout

--- express_server.js
+++ express_server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 //*************************************************//
-
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 //*************************************************//
@@ -23,6 +24,7 @@ const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
 	'9sm5xK': 'http://www.google.com'
 };
+
 //*************************************************//
 
 app.get('/', (req, res) => {
@@ -36,15 +38,20 @@ app.get('/hello', (req, res) => {
 	res.render('hello_world', templateVars);
 });
 app.get('/urls', (req, res) => {
-	const templateVars = { urls: urlDatabase };
+	const templateVars = { urls: urlDatabase,
+	username: req.cookies["username"] };
 	res.render('urls_index', templateVars);
 });
 app.get('/urls/new', (req, res) => {
-	res.render('urls_new');
+	const templateVars = {
+		username: req.cookies["username"] };
+	res.render('urls_new',templateVars);
 });
+
 //*************************************************//
 app.get('/urls/:shortURL', (req, res) => {
-	const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+	const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
+	username: req.cookies["username"] };
 	res.render('urls_show', templateVars);
 });
 app.get('/u/:shortURL', (req, res) => {
@@ -55,14 +62,32 @@ app.get('/u/:shortURL', (req, res) => {
 //*************************************************//
 
 app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString();
+	const shortURL = generateRandomString();
   const longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
   res.redirect(`/urls/${shortURL}`);
+	
+});
+//************************Login*************************//
 
+app.post('/login', (req, res) => {
+	const username = req.body.username;
+	res.cookie('username', username);
+	res.redirect('/urls');
 });
 
-//DELETE*************************************************//
+//*************************Logout************************//
+app.post('/logout', (req, res) => {
+	res.clearCookie('username');
+  res.redirect('/urls');
+});
+//*************************************************//
+app.post('/urls/:id', (req, res) => {
+  const shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect('/urls');
+});
+//*************DELETE**********************************//
 app.post('/urls/:shortURL/delete',(req, res)=>{
 	const shortURL = req.params.shortURL;
 	delete urlDatabase[shortURL];
